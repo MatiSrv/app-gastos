3c44a72f96407b844d789a8098d3d995ee8b5394
PiMatiasSiravegna <matisiravegna04@gmail.com>
2026-04-17 23:20:23 -0300

    deploy fix

--- backend/app/main.py
+++ backend/app/main.py
@@ -10,7 +10,7 @@ app.add_middleware(
         "http://localhost:5173",
         "http://127.0.0.1:5173",
         "https://app-gastos.vercel.app",
-        "https://*.vercel.app",
+        "https://app-gastos-front-five.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
